be7b58d61d2975fc47d68780adee47f8602f4e3a
HASANOV Mustafa <HE202470@students.ephec.be>
2026-03-05 14:00:32 +0100

    tp 3 ex ap 2.7.1

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import './middlewares/errorHandler'
 import {errorHandler} from "./middlewares/errorHandler";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger";
+import cors from 'cors';
 
 
 const app = express();
@@ -17,6 +18,7 @@ app.use(requestLogger);
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use(cors());
 
 
 app.get('/', (req:object, res:object) => {
